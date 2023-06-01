49c87fe2bc216bab57c703c046787dadb9df299c
BramwelMakindu <108723683+BramwelMakindu@users.noreply.github.com>
2023-06-01 13:32:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const notification = document.querySelector('.js-div');
 const message = document.querySelector('.message')
+const countEl = document.querySelector('.count');
+const newMsgs = document.querySelectorAll('.new');
+const allRead = document.querySelector('.read');
+
+let count = 3;
 
 notification.addEventListener('click', () => {
     message.classList.toggle('active');
+});
+
+newMsgs.forEach(newMsg => {
+    newMsg.addEventListener('click', ()=>{
+        count--;
+        showmsgs()
+        newMsg.classList.add('active');
+    });
+})
+
+function showmsgs(){
+    countEl.innerHTML = `${count}`;
+    if (count < 0) {
+        countEl.innerHTML = '0'
+    }
+}
+
+allRead.addEventListener('click', () => {
+    newMsgs.forEach(newMsg => {
+        newMsg.classList.add('active');
+        countEl.innerHTML = '0';
+        count = 0;
+    });
 });
